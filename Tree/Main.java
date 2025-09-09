2c7fc11e1687f4afb597cc9f1832bfab053650c0
kamarlaylatt <kamarehtha96@gmail.com>
2025-09-09 20:07:30 +0700

    Add AVLTree implementation with insertion and balancing methods; update Main to use AVLTree

--- Tree/Main.java
+++ Tree/Main.java
@@ -2,27 +2,10 @@ package Tree;
 
 public class Main {
     public static void main(String[] args) {
-        // n! = n * (n-1)!
-        Tree tree = new Tree();
-        tree.insert(7);
-        tree.insert(4);
-        tree.insert(9);
-        tree.insert(1);
-        tree.insert(6);
-        tree.insert(8);
+        var tree = new AVLTree();
         tree.insert(10);
-
-        Tree tree2 = new Tree();
-        tree2.insert(7);
-        tree2.insert(4);
-        tree2.insert(9);
-        tree2.insert(1);
-        tree2.insert(6);
-        tree2.insert(8);
-        tree2.insert(11);
-        tree.traverseLevelOrder();
-
-        // tree.swapRoot();
+        tree.insert(30);
+        tree.insert(20);
 
     }
 
