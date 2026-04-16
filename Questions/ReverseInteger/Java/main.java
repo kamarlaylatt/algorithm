93d8e801354fb4ba82036fa1c1ba4b03de301ae2
kamarlaylatt <kamarehtha96@gmail.com>
2026-04-17 00:02:26 +0700

    Add reverseInteger2 method with overflow handling

--- Questions/ReverseInteger/Java/main.java
+++ Questions/ReverseInteger/Java/main.java
@@ -19,4 +19,17 @@ public class main {
 		int output = Integer.parseInt(reverseInputs);
 	    return output;
 	}
+
+	public static int reverseInteger2(int x) {
+		int reversed = 0;
+		while (x != 0) {
+			int digit = x % 10;
+			x /= 10;
+			if (reversed > Integer.MAX_VALUE / 10 || reversed < Integer.MIN_VALUE / 10) {
+				return 0;
+			}
+			reversed = reversed * 10 + digit;
+		}
+		return reversed;
+	}
 }
